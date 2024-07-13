42594149fa951cf2672441bb9ae223b8237fac44
qu-ngx <quang13012005@gmail.com>
2024-07-12 20:30:56 -0700

    fixed typos

--- auth/server.py
+++ auth/server.py
@@ -11,7 +11,7 @@ mysql = MySQL(server)
 # config
 server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
 server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
-server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWOR")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
 server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
 server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
 
