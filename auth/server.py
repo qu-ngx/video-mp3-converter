69f6249a6d7298cdd324c2031ab91ae5c4821be2
qu-ngx <quang13012005@gmail.com>
2024-07-13 22:20:50 -0700

    added validate endpoint

--- auth/server.py
+++ auth/server.py
@@ -42,6 +42,22 @@ def login():
         return "invalid credentials", 401
 
 
+@server.route("/validate", methods=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+    if not encoded_jwt:
+        return "missing credentials", 401
+
+    encoded_jwt = encoded_jwt.split(" ")[1]
+    try:
+        decoded = jwt.decode(
+            encoded_jwt, os.environ.get("JWT_SECRET"), algorithms=["HS256"]
+        )
+    except:
+        return "not authorizated", 403
+
+    return decoded, 200
+
 def createJWT(username, secret, authz):
     return jwt.encode(
         {
