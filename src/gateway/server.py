c344f34dc54553011b2963e38a8c39cc74231ce5
qu-ngx <quang13012005@gmail.com>
2024-07-15 10:26:58 -0700

    added __init__.py for auth gateway

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -24,3 +24,13 @@ channel = connection.channel()
 @server.route("/login", methods=["POST"])
 def login():
     token, err = access.login(request)
+
+    if not err:
+        return token
+    else:
+        return err
+
+
+@server.route("/upload", methods=["POST"])
+def upload():
+    access, err = validate.token(request)
